6dafe64889d8653b5f17727ee1682e8d011d788d
arifulislam514 <ariful425616@gmail.com>
2025-07-07 23:10:57 +0600

    Redy For Deploment

--- event_management_system/settings.py
+++ event_management_system/settings.py
@@ -13,7 +13,8 @@ SECRET_KEY = 'django-insecure-=ee==1&pfuti-c)&4&iv2m_t-untzcpnquai+=i-c%(1^oh+1c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
